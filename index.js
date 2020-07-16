0dbd0ef15ab5e8eeb0ae59ec9dde2d288c891db5
Igira <eddygucci256@gmail.com>
2020-07-16 13:29:35 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+var colors = document.querySelector(".colors")
+    colors.style.color = "gold";
+    
+var h1 = document.querySelector("h1");
+h1.style.color = "rgb(61, 164, 199)";
+
+var ul = document.querySelector("ul");
+ul.style.color = "purple";
+
+document.body.style.backgroundColor = "grey";
